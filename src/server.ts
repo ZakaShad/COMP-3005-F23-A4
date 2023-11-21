01a348e0ca80c5eeb6a3b2441311b3ceb0472605
Shady Zakaria <shadyashraf2002@gmail.com>
2023-11-21 18:10:54 -0500

    post is done; delete is WOP

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,8 @@ import express, {Request, Response} from 'express';
 import { Pool } from 'pg';
 
 const application = express();
-
+const bodyParser = require('body-parser');
+application.use(bodyParser.json());
 const PORT = 1337;
 
 const pool = new Pool({
@@ -27,16 +28,60 @@ application.get("/students", async (req: Request, res: Response) => {
       }
 });
 
-application.post("/student", (req: Request, res: Response) => {
-    res.send("Posted student");
-})
+application.post("/student", async(req: Request, res: Response) => {
+    
+    try {
+        const query = `
+        INSERT INTO students(first_name, last_name, email, enrollment_date) 
+        VALUES
+        ('${req.body.first_name}', '${req.body.last_name}', '${req.body.email}', '${req.body.enrollment_date}');
+        `
 
-application.put("/student", (req: Request, res: Response) => {
+        // console.log(query);
+        const result = await pool.query(query);
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
+
+});
+
+application.put("/student", async (req: Request, res: Response) => {
+    try {
+        const query = `
+        INSERT INTO students(first_name, last_name, email, enrollment_date) 
+        VALUES
+        ('${req.body.first_name}', '${req.body.last_name}', '${req.body.email}', '${req.body.enrollment_date}');
+        `
+
+        // console.log(query);
+        const result = await pool.query(query);
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
+    
     res.send("Updated student email");
 })
 
-application.delete("/student", (req: Request, res: Response) => {
-    res.send("Deleted student");
+application.delete("/student/:student_id", async (req: Request, res: Response) => {
+    
+    try {
+        const query = `
+        DELETE FROM students 
+        WHERE student_id = ${req.paams.student_id};
+        `
+
+        // console.log(query);
+        const result = await pool.query(query);
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
+
 })
 
 application.listen(PORT, () => {
